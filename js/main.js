1f6d893b966c04fe3a70f4e2b73f7a8018cc5183
P4anix <pancok_1@yahoo.it>
2023-03-15 16:16:35 +0100

    todolist-done(first step)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 'use strict'
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            todos: [
+                {
+                    text: 'Andare a fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'Portare mia moglie a comprare qualsiasi cosa',
+                    done: true
+                },
+                {
+                    text: 'Regalare alla bambina una rosa',
+                    done: false
+                },
+                {
+                    text: 'Ubriacarsi con gli amici',
+                    done: false
+                },
+                {
+                    text: 'Giocare a Red Ded Redemption 2',
+                    done: true
+                },
+                {
+                    text: 'Portare la macchina al lavaggio',
+                    done: false
+                }
+            ]
+        }
+    }
+
+}).mount('#app')
